695384bf25f876e72a7449783a8793a09714d4e9
ravindukaluarachchi <raoffire@yahoo.com>
2015-08-13 16:46:17 +0530

    created gui

--- src/jdkversionswitch/JDKVersionSwitch.java
+++ src/jdkversionswitch/JDKVersionSwitch.java
@@ -9,15 +9,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.MapValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  *
@@ -42,16 +56,77 @@ public class JDKVersionSwitch extends Application {
         
         Scene scene = new Scene(root, 300, 250);
         
+        TableView table = new TableView();
+        table.setEditable(true);
+        
+        TableColumn<String,CheckBox> column1 = new TableColumn("");
+        //column1.setCellFactory(CheckBoxTableCell.forTableColumn(TableColumn<String, CheckBox> param) -> new CheckBoxTableCell<>());
+        column1.setCellValueFactory(new MapValueFactory("1"));
+        column1.setEditable(true);
+        column1.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<String,CheckBox>>() {
+
+            @Override
+            public void handle(TableColumn.CellEditEvent<String,CheckBox> event) {
+                System.out.println("--> " + event.getNewValue().isSelected());
+                 
+            }
+        });
+        
+        TableColumn column2 = new TableColumn("JDK Version");
+        column2.setCellFactory(TextFieldTableCell.forTableColumn());
+        column2.setCellValueFactory(new MapValueFactory("2"));
+        column2.setMinWidth(400);
+        column2.setEditable(true);
+        
+        column2.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<String,String>>() {
+
+            @Override
+            public void handle(TableColumn.CellEditEvent<String,String> event) {
+                System.out.println("--> " + event.getNewValue());
+                 
+            }
+        });
+        
+       
+        
+ 
+        
+        table.getColumns().setAll(column1,column2);
+        root.getChildren().add(table);
+        
+        
+        getData().stream().forEach((map) -> {
+            table.getItems().add(map);
+        });
+        
+        
+        primaryStage.setMinHeight(300);
+        primaryStage.setMinWidth(500);
+        primaryStage.setResizable(false);
+        
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    
+    private List<Map> getData(){
+        Map m1 = new HashMap();
+        m1.put("1", true);
+        m1.put("2", "open jdk 7");
+        
+        Map m2 = new HashMap();
+        m2.put("1", false);
+        m2.put("2", "oracle jdk 8");
+        
+        return Arrays.asList(m1,m2 );
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
+        /*try {
             Process proc = Runtime.getRuntime().exec(" update-alternatives --config javac ");
             InputStream in = proc.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -69,7 +144,7 @@ public class JDKVersionSwitch extends Application {
         } catch (IOException ex) {
             Logger.getLogger(JDKVersionSwitch.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+        */
         launch(args);
     }
     
